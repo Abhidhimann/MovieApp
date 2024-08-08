2aa64af9b6fd65937cf348560df3d51125a9aea7
Abhishek <abhishekdhimaniitg@gmail.com>
2024-08-08 19:58:24 +0900

    refactor app database class

--- app/src/main/java/com/example/movieapp/MyApp.kt
+++ app/src/main/java/com/example/movieapp/MyApp.kt
@@ -1,7 +1,6 @@
 package com.example.movieapp
 
 import android.app.Application
-import com.example.movieapp.data.local.database.AppDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 
--- app/src/main/java/com/example/movieapp/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/movieapp/data/local/database/AppDatabase.kt
@@ -1,10 +1,7 @@
 package com.example.movieapp.data.local.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.movieapp.BuildConfig
 import com.example.movieapp.data.local.entity.SavedItemEntity
 import com.example.movieapp.data.local.dao.SavedItemDao
 
@@ -13,23 +10,4 @@ import com.example.movieapp.data.local.dao.SavedItemDao
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun savedItemDao(): SavedItemDao
-
-
-    // later move it to di/modules
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    BuildConfig.DB_NAME
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
